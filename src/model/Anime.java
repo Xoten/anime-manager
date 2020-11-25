1b83dab6774e3b9821b598c10d8947f1106ddde1
Alejandro Coronel Sanchez <alejo.co9@hotmail.com>
2020-11-25 00:56:32 -0500

    modify model classes

--- src/model/Anime.java
+++ src/model/Anime.java
@@ -2,4 +2,68 @@ package model;
 
 public class Anime {
 
+
+	private String picture;
+	private String studios;
+	private int score;
+	private String type;
+	private String genres;
+	private int episodes;
+	private int currentep;
+
+
+	public Anime(String p, String st, int s, String t, String g, int ep) {
+		p = picture;
+		st = studios;
+		s = score;
+		t = type;
+		g = genres;
+		ep = episodes;
+
+
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public String getStudios() {
+		return studios;
+	}
+	public void setStudios(String s) {
+		s = studios;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getGenres() {
+		return genres;
+	}
+	public void setGenres(String genres) {
+		this.genres = genres;
+	}
+	public int getEpisodes() {
+		return episodes;
+	}
+	public void setEpisodes(int episodes) {
+		this.episodes = episodes;
+	}
+	
+	public int getCurrentep() {
+		return currentep;
+	}
+	public void setCurrentep(int currentep) {
+		this.currentep = currentep;
+	}
 }
+
--- src/model/Comic.java
+++ src/model/Comic.java
@@ -2,4 +2,61 @@ package model;
 
 public class Comic {
 
+	private int chapters;
+	private int volumes;
+	private String author;
+	private String genres;
+	private String image;
+	
+	public Comic(int c, int v, String a,String g, String i) {
+		
+	chapters = c;
+	volumes = v;
+	author = a;
+	genres = g;
+	image = i;
+		
+		
+		
+	}
+	
+	
+	public int getChapters() {
+		return chapters;
+	}
+	public void setChapters(int chapters) {
+		this.chapters = chapters;
+	}
+	public int getVolumes() {
+		return volumes;
+	}
+	public void setVolumes(int volumes) {
+		this.volumes = volumes;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getGenres() {
+		return genres;
+	}
+	public void setGenres(String genres) {
+		this.genres = genres;
+	}
+
+
+	public String getImage() {
+		return image;
+	}
+
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+	
+	
 }
